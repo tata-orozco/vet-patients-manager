795a377986c28bfee15913816308cdb8646e84c1
tata-orozco <juanofsa.1989@gmail.com>
2024-04-25 17:18:54 -0300

    Added Vet Patients Manager

---
+++ src/components/PatientDetailItem.tsx
@@ -0,0 +1,13 @@
+type PatientDetailItem = {
+    label: string,
+    data: string
+}
+
+export default function PatientDetailItem({label, data} : PatientDetailItem) {
+  return (
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+        {label}: {''}
+        <span className="font-normal normal-case">{data}</span>
+    </p>
+  )
+}
